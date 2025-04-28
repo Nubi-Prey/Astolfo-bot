41a5eac4998413e98b5a79cff410a11b8d6da4c8
Nubi-Prey <muriloalvesnp@gmail.com>
2025-04-28 15:46:25 -0300

    Se o test.mode estiver habilitado, o bot apenas funcionará no servidor de teste.

--- src/main/java/listeners/SlashCommandListener.java
+++ src/main/java/listeners/SlashCommandListener.java
@@ -4,6 +4,7 @@ import commands.AbstractCommand;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.Objects;
 import java.util.concurrent.ConcurrentMap;
 
 /**
@@ -11,15 +12,27 @@ import java.util.concurrent.ConcurrentMap;
  */
 public class SlashCommandListener extends ListenerAdapter {
     private final ConcurrentMap<String, AbstractCommand> commandRegistry;
+    private final Boolean TEST_MODE;
+    private final String TEST_GUILD_ID;
 
-    public SlashCommandListener(ConcurrentMap<String, AbstractCommand> commandRegistry) {
+    public SlashCommandListener(ConcurrentMap<String, AbstractCommand> commandRegistry, Boolean TEST_MODE, String TEST_GUILD_ID) {
         this.commandRegistry = commandRegistry;
+        this.TEST_MODE = TEST_MODE;
+        this.TEST_GUILD_ID = TEST_GUILD_ID;
     }
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
 
         String CommandName = event.getName();
+        String GuildId = Objects.requireNonNull(event.getGuild()).getId();
+
+        if(TEST_MODE && !TEST_GUILD_ID.equals(GuildId)){
+            event.reply("Bot habilitado apenas em servidor de testes.")
+                    .setEphemeral(true)
+                    .queue();
+            return;
+        }
 
         try {
             AbstractCommand command = commandRegistry.get(CommandName);
