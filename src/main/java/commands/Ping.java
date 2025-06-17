29af679a2e3c8ea600799f93afb40d3195f64dfb
Nubi-Prey <muriloalvesnp@gmail.com>
2025-06-17 09:23:41 -0300

    Comando renomeado para seguir o padrão PascalCase

--- src/main/java/commands/Ping.java
+++ src/main/java/commands/Ping.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 /**
  * Mostra a latência do bot
  */
-public class ping extends AbstractCommand{
+public class Ping extends AbstractCommand{
 
     @Override
     public SlashCommandData getCommandData(){
